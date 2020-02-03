b59ad8599cfd0f1a9d82cf909257b0762f4b49a4
Jenn Soderborg <jsoderborg7@gmail.com>
2020-02-03 15:56:01 -0700

    working on styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
+import './scss/App.scss'
 
 import World from './components/world'
 import LandingPage from './components/landing/LandingPage'
@@ -8,14 +9,7 @@ import Register from './components/landing/Register'
 
 function App() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        height: '100vh',
-        width: '100vw',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }}>
+    <div>
         <Route exact path="/" component={LandingPage} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
--- front-end/src/components/landing/LandingPage.js
+++ front-end/src/components/landing/LandingPage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import '../../scss/landing.scss'
 
 const LandingPage = () =>{
   return(
     <div className="landing-container">
+      <h1>Welcome to Team Big Bosses!</h1>
       <div className="landing-nav">
         <Link to="/login">Login</Link>
       </div>
-      <h1>Welcome to Team Big Bosses!</h1>
+      <div className='tree' />
     </div>
   )
 }
